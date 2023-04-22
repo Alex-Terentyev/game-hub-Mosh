3f13bccd33dae277ca016025c270b62e503a7c7c
Lyo-T <lyotereom>
2023-04-22 11:34:11 +0100

    Built basic responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="hotpink" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg="skyBlue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="beige">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
