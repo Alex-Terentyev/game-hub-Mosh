00141135bdee5d7c5b344abf5476f510ef9eefe6
Alexey Terentyev <alyosha@sapo.pt>
2023-04-25 11:03:52 +0100

    Add dynamic page heading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -24,7 +24,6 @@ const GameGrid = ({ gameQuery }: Props) => {
           xl: 5,
         }}
         spacing={3}
-        padding={5}
       >
         {isLoading &&
           skeletons.map((s) => (
