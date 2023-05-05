d8fde708fe9219ee262620be238f2e6d0e04e5ae
Lyo-T <lyotereom>
2023-05-05 13:30:31 +0100

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import Layout from "./pages/Layout";
 import GameGrid from "./components/GameGrid";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
