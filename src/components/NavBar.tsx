343d2103981cdc4b8684a8bdf949720531591198
Alexey Terentyev <alyosha@sapo.pt>
2023-05-06 17:28:25 +0100

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,18 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearcInput from "./SearcInput";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <HStack padding={"10px"}>
-      <Image src={logo} boxSize={"60px"} />
+      <Image
+        cursor={"pointer"}
+        src={logo}
+        boxSize={"60px"}
+        onClick={() => navigate("/")}
+      />
       <SearcInput />
       <ColorModeSwitch />
     </HStack>
